517cb33e822dd171fc659d5deaa729189fc37b64
AmonPlayer <114870004+AmonPlayer@users.noreply.github.com>
2023-03-03 17:03:58 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+alunos = {
+    'amons':
+        {
+            'name': 'amon Santos',
+            'classwork': [90, 95, 80, 10],
+            'tests': [90, 80],
+            'lab_work': [70, 85.2]
+        },
+    'pedros':
+        {
+            'name': 'pedro silva',
+            'classwork': [90, 75, 80, 10],
+            'tests': [70, 100],
+            'lab_work': [80, 90.2]
+
+        },
+    'marias':
+        {
+            'name': 'maria Santos',
+            'classwork': [90, 95, 80, 10],
+            'tests': [90, 80],
+            'lab_work': [70, 85.2]
+        },
+    'joaos':
+        {
+            'name': 'joao Santos',
+            'classwork': [90, 95, 80, 10],
+            'tests': [90, 80],
+            'lab_work': [70, 85.2]
+        },
+    'tiagos':
+        {
+            'name': 'tiago Santos',
+            'classwork': [90, 95, 80, 10],
+            'tests': [90, 80],
+            'lab_work': [70, 85.2]
+        }
+}
